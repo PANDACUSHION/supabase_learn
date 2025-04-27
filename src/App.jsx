2ec44bf2296c69527e05d96f51057efe4aea559f
Manjil Sapkota <sapkotamanish178@gmail.com>
2025-04-27 21:10:26 +0545

    small changes added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
+import Layout from './layout/Layout.jsx'
 import Home from './component/Home';
 import Create from './component/Create.jsx';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <Home />
-        <Create />
-    </>
-  )
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/add" element={<Create />} />
+            </Routes>
+        </Layout>
+    )
 }
 
 export default App
